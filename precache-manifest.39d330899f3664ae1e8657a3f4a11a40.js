d2ddd7bc9e55cdf850d51713ed968fc9accb4b72
Mitch <mitchstoffels@gmail.com>
2019-02-03 10:16:40 +0100

    Updates

---
+++ precache-manifest.39d330899f3664ae1e8657a3f4a11a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c793f4aef30bcf0f34",
+    "url": "/bustabit-script-simulator/static/css/main.983a32fe.chunk.css"
+  },
+  {
+    "revision": "c8c793f4aef30bcf0f34",
+    "url": "/bustabit-script-simulator/static/js/main.c8c793f4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/bustabit-script-simulator/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "70539ca2902cafe75a3b",
+    "url": "/bustabit-script-simulator/static/js/runtime~main.70539ca2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bustabit-script-simulator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da68736baffb1af2bf6756bd5d125722",
+    "url": "/bustabit-script-simulator/index.html"
+  }
+];
