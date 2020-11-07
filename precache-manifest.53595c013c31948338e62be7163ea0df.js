2c3326b5fb1fb5180099115a378c4a2227edf582
Mitch Stoffels <mstoffels@motto.nl>
2020-11-08 00:07:28 +0100

    Updates

---
+++ precache-manifest.53595c013c31948338e62be7163ea0df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712eba9cf70e2e7f59a33df605337195",
+    "url": "/bustabit-script-simulator/index.html"
+  },
+  {
+    "revision": "05408b2e43f8452d4d20",
+    "url": "/bustabit-script-simulator/static/css/2.263bab6e.chunk.css"
+  },
+  {
+    "revision": "b6f04ba591f8b90cc483",
+    "url": "/bustabit-script-simulator/static/css/main.655e0c3e.chunk.css"
+  },
+  {
+    "revision": "05408b2e43f8452d4d20",
+    "url": "/bustabit-script-simulator/static/js/2.0e488a5c.chunk.js"
+  },
+  {
+    "revision": "e82e140f4ac2d8761d02b09a6fe70284",
+    "url": "/bustabit-script-simulator/static/js/2.0e488a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f04ba591f8b90cc483",
+    "url": "/bustabit-script-simulator/static/js/main.f7378fd4.chunk.js"
+  },
+  {
+    "revision": "d6e2e514d6682ed99fb9",
+    "url": "/bustabit-script-simulator/static/js/runtime-main.2c96e0c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bustabit-script-simulator/precache-manifest.b66e0b6aa26f51054ce166e7e0c0f8d6.js"
+  "/bustabit-script-simulator/precache-manifest.53595c013c31948338e62be7163ea0df.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bustabit-script-simulator/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bustabit-script-simulator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
